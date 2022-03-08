0e4ce47e865a3dd01bdeea29f060a0e080359488
andreiapaixao <andreiapaixao@id.uff.br>
2022-03-07 21:55:57 -0300

    add classes

--- src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
+++ src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
@@ -2,7 +2,9 @@ package one.digitalinnovation.personapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan
 @SpringBootApplication
 public class PersonapiApplication {
 
